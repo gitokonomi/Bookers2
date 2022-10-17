e4dcfaf2a5b3ef0b7c3839cabcb17a3c86be61f1
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-10-17 13:27:31 +0000

    [Fix]投稿詳細画面リンク修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image, :introduction)
+    params.require(:user).permit(:name, :email, :profile_image, :introduction)
   end
 
 
