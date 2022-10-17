1322b5e9cdaf833c75aa29945412f059cd5e8315
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-10-17 08:28:24 +0000

    [Add]nameに一意性追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
   
-  validates :name, presence: true, length: { minimum: 2, maximum: 20}
+  validates :name, uniqueness: true, presence: true, length: { minimum: 2, maximum: 20}
   validates :introduction, length: { maximum: 50 }
 
 has_one_attached :profile_image
