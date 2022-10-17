98b9ff99a8e669be9648f410bbb094f70451f7d6
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-10-17 13:39:19 +0000

    [Add]フラッシュメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       # 編集成功時フラッシュメッセージ
-      # flash[:notice] = "You have updated user successfully."
+      flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user.id)
     else
       render :edit
